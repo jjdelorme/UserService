86809c981bb10177b6c1ef6e6a47f65a0f0d3eee
Jason De Lorme <jasondel@google.com>
2025-03-03 20:11:27 -0500

    Adding copyright statement.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This application demonstrates a couple of enterprise coding standards:
 
+1. All files contain a copyright header
 1. All DTOs are named `ObjectXyzDTO.java`
 1. All services that create, update, delete data are annotated with `@AuditLog()` whereas read methods are not.
 
--- src/main/java/com/acme/userservice/service/DivisionService.java
+++ src/main/java/com/acme/userservice/service/DivisionService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2025 ACME Corp.
+ *
+ * This work is protected by copyright law and international treaties.
+ * Unauthorized reproduction or distribution of this work, or any portion
+ * thereof, may result in severe civil and criminal penalties, and will be
+ * prosecuted to the maximum extent possible under the law.
+ *
+ * ACME Corp. Confidential. All Rights Reserved.
+ *
+ * This software contains the proprietary and confidential information of
+ * ACME Corp., and may not be copied, reproduced, or distributed in any
+ * form without the express written permission of ACME Corp.
+ */
+
 package com.acme.userservice.service;
 
 import com.acme.userservice.annotation.AuditLog;
